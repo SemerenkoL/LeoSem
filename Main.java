b40e7c7a05a250340bae237aae2d680ac5068542
SemerenkoL <92167088+SemerenkoL@users.noreply.github.com>
2021-10-08 23:45:41 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+        static int factorial(int n) {
+            int result;
+
+            if (n < 1) {
+                return 1;
+            }
+            result = factorial(n - 1) * n;
+            return result;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Факториал цисла 5: " + factorial(1));
+            System.out.println("Факториал числа 6: " + factorial(6));
+            System.out.println("Факториал числа 7: " + factorial(7));
+            System.out.println("Факториал числа 8: " + factorial(8));
+            System.out.println("Факториал числа 9: " + factorial(9));
+            System.out.println("Факториал числа 10: " + factorial(10));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
